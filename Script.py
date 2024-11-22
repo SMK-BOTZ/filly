768b842694c9d856a52ff8d5f69591b418386051
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-11-23 02:43:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>{caption}</b>""" 
+    CAPTION = """<b>{filename}</b>""" 
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
